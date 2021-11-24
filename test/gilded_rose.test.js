4f4b4acd71b6e8d54dde48e624044fdc92d8561d
Arthur Fincham <arthurfincham@me.com>
2021-11-24 13:14:19 +0000

    first commit

---
+++ test/gilded_rose.test.js
@@ -0,0 +1,25 @@
+const {Shop, Item} = require("../src/gilded_rose");
+const fs = require('fs-extra');
+const { exec } = require('child_process');
+
+
+
+describe("Gilded Rose", function() {
+  it("should foo", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("foo");
+  });
+  
+  it("should equal the Gold Standard", async(done) => {
+    const script = `node test/texttest_fixture.js 11 > test/test.txt `;
+    await exec(script, () => {
+      const gold_standard = fs.readFileSync('./test/gold_standard.txt');
+      const test_text = fs.readFileSync('./test/test.txt');
+      expect(test_text).toEqual(gold_standard);
+      done();
+    })
+  })
+});
+
+
