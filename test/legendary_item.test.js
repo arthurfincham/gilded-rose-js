7dda3bd4351a0bca9f1286f669868322bb855bec
Arthur Fincham <arthurfincham@me.com>
2021-11-24 16:34:51 +0000

    change test subject name to subject

--- test/legendary_item.test.js
+++ test/legendary_item.test.js
@@ -2,13 +2,13 @@ const MockItem = require('./mock_item')
 const LegendaryItem = require('../src/legendary_item')
 
 describe('Legendary Item', () => {
-  const legendary_item = new LegendaryItem(new MockItem("Sulfuras", 50, 80))
+  const subject = new LegendaryItem(new MockItem("Sulfuras", 50, 80))
   test('should not change its sellIn', () => {
-    legendary_item.update
-    expect(legendary_item.item.sellIn).toEqual(50)
+    subject.update
+    expect(subject.item.sellIn).toEqual(50)
   });
   test('should not change its quality', () => {
-    legendary_item.update;
-    expect(legendary_item.item.quality).toEqual(80);
+    subject.update;
+    expect(subject.item.quality).toEqual(80);
   });
 })
--- test/standard_item.test.js
+++ test/standard_item.test.js
@@ -2,15 +2,15 @@ const StandardItem = require('../src/standard_item')
 const MockItem = require('./mock_item')
 
 describe('Standard Item', () => {
-  const standard_item = new StandardItem(new MockItem('Standard', 10, 10))
+  const subject = new StandardItem(new MockItem('Standard', 10, 10))
   beforeEach(() => {
-    standard_item.update();
+    subject.update();
   })
   test('quality reduces by 1', () => {
-    expect(standard_item.item.quality).toEqual(9)
+    expect(subject.item.quality).toEqual(9)
   });
   test('sellIn reduces by 1', () => {
-    expect(standard_item.item.sellIn).toEqual(8);
+    expect(subject.item.sellIn).toEqual(8);
   });
 
   test('quality is capped at a high of 50', () => {
