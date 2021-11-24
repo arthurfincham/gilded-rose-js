02dd4a310ae10ae20e242230b92aba03bd5c5328
Arthur Fincham <arthurfincham@me.com>
2021-11-24 16:45:19 +0000

    add AgedBrie quality increase for negative sellIn

--- src/aged_brie.js
+++ src/aged_brie.js
@@ -1,8 +1,14 @@
-const StandardItem = require('./standard_item')
+const StandardItem = require('./standard_item');
 
 class AgedBrie extends StandardItem {
-  update(){
-    
+  update() {
+    if (this.item.sellIn <= 0) {
+      this.item.quality += 2;
+    } else {
+      this.item.quality += 1;
+    }
+    this.item.sellIn -= 1;
+    this.quality_cap();
   }
 }
 
--- test/aged_brie.test.js
+++ test/aged_brie.test.js
@@ -2,11 +2,22 @@ const AgedBrie = require('../src/aged_brie.js')
 const MockItem = require('./mock_item')
 
 describe('Aged Brie', () => {
-  const mock_item = new MockItem('Aged Brie', 10, 10)
+  const mock_item = new MockItem('Aged Brie', 10, 47)
+  const mock_old_item = new MockItem('Aged Brie', -10, 40);
   const subject = new AgedBrie(mock_item);
 
   test('should increase the quality of the item', () => {
-    subject.update
-    expect(subject.item.quality).toEqual(11)
+    subject.update()
+    expect(subject.item.quality).toEqual(48)
+  });
+  test('should cap quality at 50', () => {
+    subject.update()
+    subject.update()
+    expect(subject.item.quality).toEqual(50)
+  });
+  const old_subject = new AgedBrie(mock_old_item);
+  test('should increase the quality by 2 if sellIn is past', () => {
+    old_subject.update();
+    expect(old_subject.item.quality).toEqual(42);
   });
 });
