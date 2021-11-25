87334d8fc79c1aaaa49858caeac3e3082514bc76
Arthur Fincham <arthurfincham@me.com>
2021-11-25 10:01:40 +0000

    DRY legendary item spec

--- test/legendary_item.test.js
+++ test/legendary_item.test.js
@@ -1,14 +1,16 @@
-const MockItem = require('./mock_item')
-const LegendaryItem = require('../src/legendary_item')
+const MockItem = require('./mock_item');
+const LegendaryItem = require('../src/legendary_item');
 
 describe('Legendary Item', () => {
-  const subject = new LegendaryItem(new MockItem("Sulfuras", 50, 80))
+  let subject;
+  beforeEach(() => {
+    subject = new LegendaryItem(new MockItem('Sulfuras', 50, 80));
+    subject.update();
+  });
   test('should not change its sellIn', () => {
-    subject.update
-    expect(subject.item.sellIn).toEqual(50)
+    expect(subject.item.sellIn).toEqual(50);
   });
   test('should not change its quality', () => {
-    subject.update;
     expect(subject.item.quality).toEqual(80);
   });
-})
+});
