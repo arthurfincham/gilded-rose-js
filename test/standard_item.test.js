178afd0e4c1460f36a109fc21cf522322db9a0a2
Arthur Fincham <arthurfincham@me.com>
2021-11-25 10:01:46 +0000

    DRY standard_item spec

--- test/standard_item.test.js
+++ test/standard_item.test.js
@@ -1,22 +1,23 @@
-const StandardItem = require('../src/standard_item')
-const MockItem = require('./mock_item')
+const StandardItem = require('../src/standard_item');
+const MockItem = require('./mock_item');
 
 describe('Standard Item', () => {
-  const subject = new StandardItem(new MockItem('Standard', 10, 10))
+  let subject;
   beforeEach(() => {
+    subject = new StandardItem(new MockItem('Standard', 10, 10));
     subject.update();
-  })
+  });
   test('quality reduces by 1', () => {
-    expect(subject.item.quality).toEqual(9)
+    expect(subject.item.quality).toEqual(9);
   });
   test('sellIn reduces by 1', () => {
-    expect(subject.item.sellIn).toEqual(8);
+    expect(subject.item.sellIn).toEqual(9);
   });
 
   test('quality is capped at a high of 50', () => {
     const high_item = new StandardItem(new MockItem('Standard', 10, 60));
     high_item.update();
-    expect(high_item.item.quality).toEqual(50)
+    expect(high_item.item.quality).toEqual(50);
   });
 
   test('quality is capped at a low of 0', () => {
@@ -24,5 +25,4 @@ describe('Standard Item', () => {
     low_item.update();
     expect(low_item.item.quality).toEqual(0);
   });
-  
 });
